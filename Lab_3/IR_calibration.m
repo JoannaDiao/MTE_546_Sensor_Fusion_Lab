59bff96e47f7ed8d06eb07020707a1eb49c3ea33
Jillian <jcwong326@gmail.com>
2023-04-04 22:46:14 -0400

    IR and fused system

--- Lab_3/IR_calibration.m
+++ Lab_3/IR_calibration.m
@@ -1,28 +1,117 @@
 clear; close all; clc;
+half_blk = 1.8;
+x_offset = 30;
+y_offset = 20;
+x = 0:0.1:10;
+y = 0:0.1:10;
+[X,Y] = meshgrid(x,y);
 
+%% Training Data
 for i = 1:9 
     load(sprintf("position_%d.mat", i))
-    short5(i) = mean(short_IR(data(:,5)));
+    % positions 1 2 3, x direction, 27 24 20
+    short5(i) = mean(short5_IR(data(:,5)));
+    short5_std(i) = std(short5_IR(data(:,5)));
+    % positions 7 8 9, x direction
     long6(i) = mean(long_IR(data(:,6)));
-    short7(i) = mean(short_IR(data(:,7)));
+    long6_std(i) = std(long_IR(data(:,6)));
+%     % positions 1 4 7, y direction, 10 13 16
+    short7(i) = mean(short7_IR(data(:,7)));
+    short7_std(i) = std(short7_IR(data(:,7)));
+%     % positions 3 6 9, y direction
     medium8(i) = mean(med_IR(data(:,8)));
+    medium8_std(i) = std(med_IR(data(:,8)));
 end
 
-% IR Sensor Voltage to Distance Functions
-function dist = short_IR(x)
-    for i = 1:100
-        dist(i) = real((18.97 + sqrt(x(i)*-111.96+329.20))/(2*(x(i)+0.2739)));
-    end
+std5 = mean(short5_std(1:3));
+std7 = mean(long6_std(7:9));
+std6 = (short7_std(1)+short7_std(4)+short7_std(7))/3;
+std8 = (medium8_std(3)+medium8_std(6)+medium8_std(9))/3;
+
+gaussian_short5 = gaussmf(X,[x_offset-short5(1)-half_blk short5_std(1)]);
+gaussian_short7 = gaussmf(Y,[y_offset-short7(1)-half_blk short7_std(1)]);
+
+gaussian_long6 = gaussmf(X,[x_offset-long6(9)-half_blk long6_std(9)]);
+gaussian_medium8 = gaussmf(Y,[y_offset-medium8(9)-half_blk medium8_std(9)]);
+
+gaussian_xy = gaussian_short5 .* gaussian_short7;
+
+% figure();
+% surf(X,Y,gaussian_short5/sum(gaussian_short5,'all'));
+% surf(X,Y,gaussian_xy/sum(gaussian_xy,'all'))
+
+disp([short5(1) short5(2) short5(3)])
+disp([long6(7) long6(8) long6(9)])
+disp([short7(1) short7(4) short7(7)])
+disp([medium8(3) medium8(6) medium8(9)])
+
+%% Test positions
+load("test_3.mat")
+figure();
+short5 = mean(rmoutliers(short5_IR(data(:,5)),"mean"));
+if short5 < 0 || short5 > 40
+    gaussian_short5 = 1;
+else
+    gaussian_short5 = gaussmf(X,[x_offset-short5-half_blk std5]);
+    subplot(2,2,1)
+    surf(X,Y,gaussian_short5/sum(gaussian_short5,'all'));
 end
 
-function dist = med_IR(x)
-    for i = 1:100
-        dist(i) = real((22.28 + sqrt(x(i)*-155.36+527.83))/(2*(x(i)-0.2023)));
+long6 = mean(rmoutliers(long_IR(data(:,6)),"mean"));
+if long6 < 0 || long6 > 40
+    gaussian_long6 = 1;
+else
+    gaussian_long6 = gaussmf(X,[x_offset-long6-half_blk std6]);
+    subplot(2,2,2)
+    surf(X,Y,gaussian_long6/sum(gaussian_long6,'all'));
+end
+
+short7 = mean(rmoutliers(short5_IR(data(:,7)),"mean"));
+if short7 < 0 || short7 > 40
+    gaussian_short7 = 1;
+else
+    gaussian_short7 = gaussmf(Y,[y_offset-short7-half_blk std7]);
+    subplot(2,2,3)
+    surf(X,Y,gaussian_short7/sum(gaussian_short7,'all'));
+end
+
+medium8 = mean(rmoutliers(med_IR(data(:,8)),"mean"));
+if medium8 < 0 || medium8 > 40
+    gaussian_medium8 = 1;
+else
+    gaussian_medium8 = gaussmf(Y,[y_offset-medium8-half_blk std8]);
+    subplot(2,2,4)
+    surf(X,Y,gaussian_medium8/sum(gaussian_medium8,'all'));
+end
+
+gaussian_fused = gaussian_short5 .* gaussian_long6 .* gaussian_short7 .* gaussian_medium8;
+
+figure();
+surf(X,Y,gaussian_fused/sum(gaussian_fused,'all'))
+
+
+%% IR Sensor Voltage to Distance Functions
+function dist = short5_IR(x)
+    for i = 1:length(x)
+        dist(i) = real((39.97 + sqrt(x(i)*-785.96+530.20))/(2*(x(i)+0.2039)));
     end
 end
 
 function dist = long_IR(x)
-    for i = 1:100
-        dist(i) = real((88.4 - sqrt(x(i)*-2530+7242.27))/(2*(x(i)-0.2262)));
+    for i = 1:length(x)
+        dist(i) = real((65.4 + sqrt(x(i)*-1750+5442.27))/(2*(x(i)-0.2462)));
     end
 end
+
+function dist = short7_IR(x)
+    for i = 1:length(x)
+        dist(i) = real((8.97 + sqrt(x(i)*-385.96+710.20))/(2*(x(i)+0.5639)));
+    end
+end
+
+function dist = med_IR(x)
+    for i = 1:length(x)
+        dist(i) = real((27.28 + sqrt(x(i)*-48.36+520.83))/(2*(x(i)-0.1023)));
+    end
+end
+
--- Lab_3/fused.m
+++ Lab_3/fused.m
@@ -1,5 +1,4 @@
-clear;
-
+clear; close all; clc;
 p1 = load('position_1.mat');
 p2 = load('position_2.mat');
 p3 = load('position_3.mat');
@@ -14,6 +13,32 @@ t2 = load('test_2.mat');
 t3 = load('test_3.mat');
 
 L = 10.0/3.0;
+half_blk = 1.8;
+x_offset = 30;
+y_offset = 20;
+
+%% IR Training
+for i = 1:9 
+    load(sprintf("position_%d.mat", i))
+    % positions 1 2 3, x direction, 27 24 20
+    short5(i) = mean(short5_IR(data(:,5)));
+    short5_std(i) = std(short5_IR(data(:,5)));
+    % positions 7 8 9, x direction
+    long6(i) = mean(long_IR(data(:,6)));
+    long6_std(i) = std(long_IR(data(:,6)));
+%     % positions 1 4 7, y direction, 10 13 16
+    short7(i) = mean(short7_IR(data(:,7)));
+    short7_std(i) = std(short7_IR(data(:,7)));
+%     % positions 3 6 9, y direction
+    medium8(i) = mean(med_IR(data(:,8)));
+    medium8_std(i) = std(med_IR(data(:,8)));
+end
+
+std5 = mean(short5_std(1:3));
+std7 = mean(long6_std(7:9));
+std6 = (short7_std(1)+short7_std(4)+short7_std(7))/3;
+std8 = (medium8_std(3)+medium8_std(6)+medium8_std(9))/3;
+
 %% thermocouple models
 % Sensor 0
 dist_0 = [0; L; 2*L; L; sqrt(2)*L; sqrt(5)*L; 2*L; sqrt(5)*L; sqrt(8)*L];
@@ -22,9 +47,6 @@ dist_0 = [0; L; 2*L; L; sqrt(2)*L; sqrt(5)*L; 2*L; sqrt(5)*L; sqrt(8)*L];
 V_0 = [v1; v2; v3; v4; v5; v6; v7; v8; v9];
 temp_0 = (V_0 - 1.25) / 0.005;
 therm_0_f = fit(temp_0, dist_0, 'poly1') % temperature to distance function
-figure(1);
-plot(therm_0_f,temp_0,dist_0, 'o');
-title("sensor 0");
 
 % Sensor 1
 dist_1 = [2*L; L; 0; sqrt(5)*L; sqrt(2)*L; L; sqrt(8)*L; sqrt(5)*L; 2*L];
@@ -33,9 +55,6 @@ therm_1_mean = mean(rmoutliers(p1.data(:,2), "mean"));
 V_1 = [mean(p1.data(:,2)); mean(p2.data(:,2)); mean(p3.data(:,2)); mean(p4.data(:,2)); mean(p5.data(:,2)); mean(p6.data(:,2)); mean(p7.data(:,2)); mean(p8.data(:,2)); mean(p9.data(:,2))];
 temp_1 = (V_1 - 1.25) / 0.005;
 therm_1_f = fit(temp_1, dist_1, 'poly1')
-figure(2);
-plot(therm_1_f,temp_1,dist_1, 'o');
-title("sensor 1");
 
 % Sensor 2
 dist_2 = [2*L; sqrt(5)*L; sqrt(8)*L; L; sqrt(2)*L; sqrt(5)*L; 0; L; 2*L];
@@ -43,9 +62,6 @@ dist_2 = [2*L; sqrt(5)*L; sqrt(8)*L; L; sqrt(2)*L; sqrt(5)*L; 0; L; 2*L];
 V_2 = [mean(p1.data(:,3)); mean(p2.data(:,3)); mean(p3.data(:,3)); mean(p4.data(:,3)); mean(p5.data(:,3)); mean(p6.data(:,3)); mean(p7.data(:,3)); mean(p8.data(:,3)); mean(p9.data(:,3))];
 temp_2 = (V_2 - 1.25) / 0.005;
 therm_2_f = fit(temp_2, dist_2, 'poly1')
-figure(3);
-plot(therm_2_f,temp_2,dist_2, 'o');
-title("sensor 2");
 
 % Sensor 3
 dist_3 = [sqrt(8)*L; sqrt(5)*L; 2*L; sqrt(5)*L; sqrt(2)*L; L; 2*L; L; 0];
@@ -53,9 +69,6 @@ dist_3 = [sqrt(8)*L; sqrt(5)*L; 2*L; sqrt(5)*L; sqrt(2)*L; L; 2*L; L; 0];
 V_3 = [mean(p1.data(:,4)); mean(p2.data(:,4)); mean(p3.data(:,4)); mean(p4.data(:,4)); mean(p5.data(:,4)); mean(p6.data(:,4)); mean(p7.data(:,4)); mean(p8.data(:,4)); mean(p9.data(:,4))];
 temp_3 = (V_3 - 1.25) / 0.005;
 therm_3_f = fit(temp_3, dist_3, 'poly1')
-figure(4);
-plot(therm_3_f, temp_3, dist_3, 'o');
-title("sensor 3");
 
 %% Thermocouple likelihood function - test 1
 x = 0:0.1:10;
@@ -110,9 +123,47 @@ distance_to_sensor = sqrt((X-sensor_x).^2 + (Y-sensor_y).^2);
 
 gaussian_xy_3 = gaussmf(distance_to_sensor,[therm_3_var therm_3_dist]);
 
-fused_therm_t1 = gaussian_xy_0 .* gaussian_xy_1 .* gaussian_xy_2 .* gaussian_xy_3;
+% IR Sensor
 figure();
-surf(X,Y,fused_therm_t1/sum(fused_therm_t1,'all'))
+short5 = mean(rmoutliers(short5_IR(t1.data(:,5)),"mean"));
+if short5 < 0 || short5 > 40
+    gaussian_short5 = 1;
+else
+    gaussian_short5 = gaussmf(X,[x_offset-short5-half_blk std5]);
+    subplot(2,2,1)
+    surf(X,Y,gaussian_short5/sum(gaussian_short5,'all'));
+end
+
+long6 = mean(rmoutliers(long_IR(t1.data(:,6)),"mean"));
+if long6 < 0 || long6 > 40
+    gaussian_long6 = 1;
+else
+    gaussian_long6 = gaussmf(X,[x_offset-long6-half_blk std6]);
+    subplot(2,2,2)
+    surf(X,Y,gaussian_long6/sum(gaussian_long6,'all'));
+end
+
+short7 = mean(rmoutliers(short5_IR(t1.data(:,7)),"mean"));
+if short7 < 0 || short7 > 40
+    gaussian_short7 = 1;
+else
+    gaussian_short7 = gaussmf(Y,[y_offset-short7-half_blk std7]);
+    subplot(2,2,3)
+    surf(X,Y,gaussian_short7/sum(gaussian_short7,'all'));
+end
+
+medium8 = mean(rmoutliers(med_IR(t1.data(:,8)),"mean"));
+if medium8 < 0 || medium8 > 40
+    gaussian_medium8 = 1;
+else
+    gaussian_medium8 = gaussmf(Y,[y_offset-medium8-half_blk std8]);
+    subplot(2,2,4)
+    surf(X,Y,gaussian_medium8/sum(gaussian_medium8,'all'));
+end
+
+fused_all_t1 = gaussian_xy_0 .* gaussian_xy_1 .* gaussian_xy_2 .* gaussian_xy_3 .* gaussian_short5 .* gaussian_long6 .* gaussian_short7 .* gaussian_medium8;
+figure();
+surf(X,Y,fused_all_t1/sum(fused_all_t1,'all'))
 xlabel('X'); ylabel('Y'); title('Test Position 1','Interpreter','Latex')
 
 %% Thermocouple likelihood function - test 2
@@ -168,9 +219,48 @@ distance_to_sensor = sqrt((X-sensor_x).^2 + (Y-sensor_y).^2);
 
 gaussian_xy_3 = gaussmf(distance_to_sensor,[therm_3_var therm_3_dist]);
 
-fused_therm_t2 = gaussian_xy_0 .* gaussian_xy_1 .* gaussian_xy_2 .* gaussian_xy_3;
+% IR Sensor
 figure();
-surf(X,Y,fused_therm_t2/sum(fused_therm_t2,'all'))
+short5 = mean(rmoutliers(short5_IR(t2.data(:,5)),"mean"));
+if short5 < 0 || short5 > 40
+    gaussian_short5 = 1;
+else
+    gaussian_short5 = gaussmf(X,[x_offset-short5-half_blk std5]);
+    subplot(2,2,1)
+    surf(X,Y,gaussian_short5/sum(gaussian_short5,'all'));
+end
+
+long6 = mean(rmoutliers(long_IR(t2.data(:,6)),"mean"));
+if long6 < 0 || long6 > 40
+    gaussian_long6 = 1;
+else
+    gaussian_long6 = gaussmf(X,[x_offset-long6-half_blk std6]);
+    subplot(2,2,2)
+    surf(X,Y,gaussian_long6/sum(gaussian_long6,'all'));
+end
+
+short7 = mean(rmoutliers(short5_IR(t2.data(:,7)),"mean"));
+if short7 < 0 || short7 > 40
+    gaussian_short7 = 1;
+else
+    gaussian_short7 = gaussmf(Y,[y_offset-short7-half_blk std7]);
+    subplot(2,2,3)
+    surf(X,Y,gaussian_short7/sum(gaussian_short7,'all'));
+end
+
+medium8 = mean(rmoutliers(med_IR(t2.data(:,8)),"mean"));
+if medium8 < 0 || medium8 > 40
+    gaussian_medium8 = 1;
+else
+    gaussian_medium8 = gaussmf(Y,[y_offset-medium8-half_blk std8]);
+    subplot(2,2,4)
+    surf(X,Y,gaussian_medium8/sum(gaussian_medium8,'all'));
+end
+
+
+fused_all_t2 = gaussian_xy_0 .* gaussian_xy_1 .* gaussian_xy_2 .* gaussian_xy_3 .* gaussian_short5 .* gaussian_long6 .* gaussian_short7 .* gaussian_medium8;
+figure();
+surf(X,Y,fused_all_t2/sum(fused_all_t2,'all'))
 xlabel('X'); ylabel('Y'); title('Test Position 2','Interpreter','Latex')
 
 %% Thermocouple likelihood function - test 3
@@ -226,9 +316,48 @@ distance_to_sensor = sqrt((X-sensor_x).^2 + (Y-sensor_y).^2);
 
 gaussian_xy_3 = gaussmf(distance_to_sensor,[therm_3_var therm_3_dist]);
 
-fused_therm_t3 = gaussian_xy_0 .* gaussian_xy_1 .* gaussian_xy_2 .* gaussian_xy_3;
+% IR Sensor
 figure();
-surf(X,Y,fused_therm_t3/sum(fused_therm_t3,'all'))
+short5 = mean(rmoutliers(short5_IR(t3.data(:,5)),"mean"));
+if short5 < 0 || short5 > 40
+    gaussian_short5 = 1;
+else
+    gaussian_short5 = gaussmf(X,[x_offset-short5-half_blk std5]);
+    subplot(2,2,1)
+    surf(X,Y,gaussian_short5/sum(gaussian_short5,'all'));
+end
+
+long6 = mean(rmoutliers(long_IR(t3.data(:,6)),"mean"));
+if long6 < 0 || long6 > 40
+    gaussian_long6 = 1;
+else
+    gaussian_long6 = gaussmf(X,[x_offset-long6-half_blk std6]);
+    subplot(2,2,2)
+    surf(X,Y,gaussian_long6/sum(gaussian_long6,'all'));
+end
+
+short7 = mean(rmoutliers(short5_IR(t3.data(:,7)),"mean"));
+if short7 < 0 || short7 > 40
+    gaussian_short7 = 1;
+else
+    gaussian_short7 = gaussmf(Y,[y_offset-short7-half_blk std7]);
+    subplot(2,2,3)
+    surf(X,Y,gaussian_short7/sum(gaussian_short7,'all'));
+end
+
+medium8 = mean(rmoutliers(med_IR(t3.data(:,8)),"mean"));
+if medium8 < 0 || medium8 > 40
+    gaussian_medium8 = 1;
+else
+    gaussian_medium8 = gaussmf(Y,[y_offset-medium8-half_blk std8]);
+    subplot(2,2,4)
+    surf(X,Y,gaussian_medium8/sum(gaussian_medium8,'all'));
+end
+
+
+fused_all_t3 = gaussian_xy_0 .* gaussian_xy_1 .* gaussian_xy_2 .* gaussian_xy_3 .* gaussian_short5 .* gaussian_long6 .* gaussian_short7 .* gaussian_medium8;
+figure();
+surf(X,Y,fused_all_t3/sum(fused_all_t3,'all'))
 xlabel('X'); ylabel('Y'); title('Test Position 3','Interpreter','Latex')
 
 %% functions
@@ -242,4 +371,28 @@ function [v1, v2, v3, v4, v5, v6, v7, v8, v9] = myfun(sensor_num,p1,p2,p3,p4,p5,
     v7 = mean(rmoutliers(p7.data(:,sensor_num), "mean"));
     v8 = mean(rmoutliers(p8.data(:,sensor_num), "mean"));
     v9 = mean(rmoutliers(p9.data(:,sensor_num), "mean"));
+end
+
+function dist = short5_IR(x)
+    for i = 1:length(x)
+        dist(i) = real((39.97 + sqrt(x(i)*-785.96+530.20))/(2*(x(i)+0.2039)));
+    end
+end
+
+function dist = long_IR(x)
+    for i = 1:length(x)
+        dist(i) = real((68.4 + sqrt(x(i)*-1750+5442.27))/(2*(x(i)-0.2462)));
+    end
+end
+
+function dist = short7_IR(x)
+    for i = 1:length(x)
+        dist(i) = real((10.97 + sqrt(x(i)*-385.96+710.20))/(2*(x(i)+0.5639)));
+    end
+end
+
+function dist = med_IR(x)
+    for i = 1:length(x)
+        dist(i) = real((27.28 + sqrt(x(i)*-48.36+520.83))/(2*(x(i)-0.1023)));
+    end
 end
